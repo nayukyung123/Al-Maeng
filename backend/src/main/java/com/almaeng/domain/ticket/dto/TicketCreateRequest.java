fcba5cad1cd7d2a163c0f2b9eb26bc056239e9fe
ParkSeoYoung <psy06004@gmail.com>
2026-03-17 17:35:22 +0900

    [S14P21B207-95] fix: dto 누락된 값 추가

--- backend/src/main/java/com/almaeng/domain/ticket/dto/TicketCreateRequest.java
+++ backend/src/main/java/com/almaeng/domain/ticket/dto/TicketCreateRequest.java
@@ -1,7 +1,10 @@
 package com.almaeng.domain.ticket.dto;
 
+import java.time.LocalDateTime;
+
 public record TicketCreateRequest(
         Long bookId,
+        LocalDateTime completedAt,
         String comment,
         String ticketImageUrl
 ) {
