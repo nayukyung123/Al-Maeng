c7a5c99e4d39b02ebc72a34a793eb059508b45b0
ParkSeoYoung <psy06004@gmail.com>
2026-03-18 13:34:20 +0900

    [S14P21B207-95] fix: 완독 도서 삭제 시 티켓 존재 여부 확인 로직 추가

--- backend/src/main/java/com/almaeng/domain/completedbook/service/CompletedBookService.java
+++ backend/src/main/java/com/almaeng/domain/completedbook/service/CompletedBookService.java
@@ -6,6 +6,7 @@ import com.almaeng.domain.completedbook.dto.CompletedBookAddRequest;
 import com.almaeng.domain.completedbook.dto.CompletedBookResponse;
 import com.almaeng.domain.completedbook.entity.CompletedBook;
 import com.almaeng.domain.completedbook.repository.CompletedBookRepository;
+import com.almaeng.domain.ticket.repository.TicketRepository;
 import com.almaeng.domain.user.entity.User;
 import com.almaeng.domain.user.repository.UserRepository;
 import com.almaeng.global.error.ApiException;
@@ -25,6 +26,7 @@ public class CompletedBookService {
     private final CompletedBookRepository completedBookRepository;
     private final UserRepository userRepository;
     private final BookRepository bookRepository;
+    private final TicketRepository ticketRepository;
 
     // 완독 도서 추가
     @Transactional
@@ -63,7 +65,9 @@ public class CompletedBookService {
         CompletedBook completedBook = completedBookRepository.findByUserIdAndBookId(userId, bookId)
                 .orElseThrow(() -> new ApiException(ErrorCode.COMPLETED_BOOK_NOT_FOUND));
 
-        // 티켓 레포지토리 생성 시 티켓 만들어져 있으면 삭제 못하도록 하는 기능 추가
+        if(ticketRepository.existsByCompletedBookId(completedBook.getId())) {
+            throw new ApiException(ErrorCode.COMPLETED_BOOK_HAS_TICKET);
+        }
 
         completedBookRepository.delete(completedBook);
     }
--- backend/src/main/java/com/almaeng/global/error/ErrorCode.java
+++ backend/src/main/java/com/almaeng/global/error/ErrorCode.java
@@ -28,7 +28,8 @@ public enum ErrorCode {
     COMPLETED_BOOK_NOT_FOUND(HttpStatus.NOT_FOUND, "T002", "완독 리스트에 존재하지 않는 도서입니다."),
     TICKET_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "T003", "이미 티켓이 발급된 도서입니다."),
     TICKET_NOT_FOUND(HttpStatus.NOT_FOUND, "T004", "존재하지 않는 티켓입니다."),
-    TICKET_ACCESS_DENIED(HttpStatus.FORBIDDEN, "T005", "해당 티켓에 대한 접근 권한이 없습니다.");
+    TICKET_ACCESS_DENIED(HttpStatus.FORBIDDEN, "T005", "해당 티켓에 대한 접근 권한이 없습니다."),
+    COMPLETED_BOOK_HAS_TICKET(HttpStatus.CONFLICT, "T006", "발급된 티켓이 존재하여 삭제할 수 없습니다.");
 
     private final HttpStatus status;
     private final String code;
