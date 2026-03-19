8189036ad24e7d2b3ae607e3dd3c1917ee7a026c
KimJoonYi <wnsawol@naver.com>
2026-03-19 16:52:17 +0900

    [S14P21B207-102] fix: TierRepository 인터페이스 전환

--- backend/src/main/java/com/almaeng/domain/user/repository/TierRepository.java
+++ backend/src/main/java/com/almaeng/domain/user/repository/TierRepository.java
@@ -1,4 +1,8 @@
 package com.almaeng.domain.user.repository;
 
-public class TierRepository {
+import com.almaeng.domain.user.entity.Tier;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TierRepository extends JpaRepository<Tier, Integer> {
+
 }
