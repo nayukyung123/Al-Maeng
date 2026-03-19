a51ebf2a53630010093e93a0d9fd5b10902e3b3f
KimJoonYi <wnsawol@naver.com>
2026-03-19 17:28:06 +0900

    [S14P21B207-102] fix: ReviewController 공통 경로 누락 수정

--- backend/src/main/java/com/almaeng/domain/review/controller/ReviewController.java
+++ backend/src/main/java/com/almaeng/domain/review/controller/ReviewController.java
@@ -15,7 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/books")
 @RequiredArgsConstructor
 public class ReviewController {
 
