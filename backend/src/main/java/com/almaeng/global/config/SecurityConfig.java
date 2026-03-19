e0e12c14e3695df902c6d8fb18f5dd9051830018
KimJoonYi <wnsawol@naver.com>
2026-03-19 17:20:59 +0900

    [S14P21B207-102] feat: 리뷰 조회는 로그인 없이도 볼 수 있도록 권한 수정

--- backend/src/main/java/com/almaeng/global/config/SecurityConfig.java
+++ backend/src/main/java/com/almaeng/global/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.almaeng.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -21,6 +22,7 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/login/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/books/*/review").permitAll()
                         .anyRequest().authenticated()
                 );
 
