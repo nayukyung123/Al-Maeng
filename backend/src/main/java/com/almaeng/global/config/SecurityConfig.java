0be7f622b547e3a9649aa4559ddf2e11ba9a90de
KimJoonYi <wnsawol@naver.com>
2026-03-20 16:48:49 +0900

    [S14P21B207-108] feat: SecurityConfig에 API 명세서 기준 Anyone 권한 허용

--- backend/src/main/java/com/almaeng/global/config/SecurityConfig.java
+++ backend/src/main/java/com/almaeng/global/config/SecurityConfig.java
@@ -27,8 +27,22 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.GET, "/api/auth/check-nickname", "/api/books/*/review").permitAll()
+                        // 인증 및 API 문서화 관련
                         .requestMatchers("/api/auth/login/**","/api/auth/signup", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+
+                        // 누구나 접근 가능한 GET API
+                        .requestMatchers(HttpMethod.GET,
+                                "/api/auth/check-nickname",
+                                "/api/books/*/review",            // 리뷰 조회
+                                "/api/banners",                   // 상단 배너 조회
+                                "/api/books/suggestions",         // 도서 검색어 자동완성
+                                "/api/keywords/rankings",         // 실시간 검색 랭킹
+                                "/api/books",                     // 도서 검색 결과
+                                "/api/books/rankings",            // 인기 도서 랭킹
+                                "/api/recommendations/contents"   // 컨텐츠 크로스 추천
+                                ).permitAll()
+
+                        // 그 외의 요청은 인증 필요
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
