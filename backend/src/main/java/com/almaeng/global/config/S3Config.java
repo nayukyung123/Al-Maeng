29054e8d5dee0e9f95aa2d0d65544ab6ca5ef75e
ParkSeoYoung <psy06004@gmail.com>
2026-03-20 13:34:37 +0900

    [S14P21B207-109] feat: 백엔드가 S3를 조작할 수 있게 만드는 Bean 추가(이미지 삭제를 위해)

--- backend/src/main/java/com/almaeng/global/config/S3Config.java
+++ backend/src/main/java/com/almaeng/global/config/S3Config.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
@@ -24,6 +25,15 @@ public class S3Config {
     public S3Presigner s3Presigner() {
         AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
         return S3Presigner.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .build();
+    }
+
+    @Bean
+    public S3Client s3Client() {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+        return S3Client.builder()
                 .region(Region.of(region))
                 .credentialsProvider(StaticCredentialsProvider.create(credentials))
                 .build();
