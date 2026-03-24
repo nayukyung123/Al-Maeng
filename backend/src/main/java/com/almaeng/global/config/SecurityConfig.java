d8a9e42a04062a11338991638edd4241fda39fb5
nayukyung <nayoukyenog@gmail.com>
2026-03-25 02:13:34 +0900

    [S14P21B207-150] feat: 비로그인 유저도 유사 도서 추천 받게 끔 경로 추가

--- backend/src/main/java/com/almaeng/global/config/SecurityConfig.java
+++ backend/src/main/java/com/almaeng/global/config/SecurityConfig.java
@@ -55,6 +55,7 @@ public class SecurityConfig {
                                                                 "/api/keywords/rankings", // 실시간 검색 랭킹
                                                                 "/api/books", // 도서 검색 결과
                                                                 "/api/books/*", // 도서 상세 조회 ({slug} 대응)
+                                                                "/api/books/*/recommendations", // 유사 도서 추천 조회
                                                                 "/api/books/rankings", // 인기 도서 랭킹
                                                                 "/api/recommendations/contents" // 컨텐츠 크로스 추천
                                                 ).permitAll()
