2627b5aebdfbfe1e6d30ff523db17ba629a53d52
nayukyung <nayoukyenog@gmail.com>
2026-03-25 11:01:24 +0900

    [S14P21B207-85] fix : 회원가입 시 사용자도서취향 저장 테이블 변경 (user_taste_report_genres → users_genres)

---
+++ backend/src/main/java/com/almaeng/domain/user/entity/UserGenreId.java
@@ -0,0 +1,16 @@
+package com.almaeng.domain.user.entity;
+
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class UserGenreId implements Serializable {
+    private Long user;
+    private Long genre;
+}
